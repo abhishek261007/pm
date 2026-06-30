c8099502703a56cd73945979dbfe08020ae415ca
abhishek2610007 <abhishekjain261007@gmail.com>
2026-06-30 20:38:48 +0530

    Remove text from catalogue cards

--- src/components/home/PdfCard.jsx
+++ src/components/home/PdfCard.jsx
@@ -1,6 +1,6 @@
 import { pdfCoverUrl } from '../../utils/media';
 
-export default function PdfCard({ file, label, category, weight }) {
+export default function PdfCard({ file, label }) {
   return (
     <a
       href={pdfCoverUrl(file)}
@@ -10,10 +10,6 @@ export default function PdfCard({ file, label, category, weight }) {
     >
       <div className="catalogue-thumb">
         <img className="catalogue-thumb-img" src={pdfCoverUrl(file)} alt={label} loading="lazy" />
-      </div>
-      <div className="catalogue-body">
-        <span className="catalogue-title">{label}</span>
-        <span className="catalogue-weight">{weight}g</span>
       </div>
     </a>
   );
